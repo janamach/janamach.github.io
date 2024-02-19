7d9f3b0f59adb4931a5c817fb790f3e5232d10b5
Jana Mach <janajg@gmail.com>
2024-02-19 09:22:18 +0100

    Site updated at Mo 19. Feb 09:22:18 CET 2024

--- home/update-site.sh
+++ home/update-site.sh
@@ -2,4 +2,4 @@ cd ~/src/janamach.github.io/
 rsync -av --delete ~/src/janamach.github.io-src/_site/ ~/src/janamach.github.io/home/
 git add .
 git commit -m "Site updated at $(date)"
-git push -f origin master
+git push -f
